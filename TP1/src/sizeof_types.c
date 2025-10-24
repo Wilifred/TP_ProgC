0c9a1384a9c2d15c784145ff5ea8144d398e5ca2
Wilifred <127045260+Wilifred@users.noreply.github.com>
2025-10-24 10:12:30 +0200

    Update sizeof_types.c

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    printf("char", sizeof(char));
+    printf("short", sizeof(short));
+    printf("int", sizeof(int));
+    printf("long int)", sizeof(long int));
+    printf("float", sizeof(float));
+    printf("double", sizeof(double));
+    printf("long double", sizeof(long double));
+}
