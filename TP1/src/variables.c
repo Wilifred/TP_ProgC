d00c7eacde3a7e2497a04ed8defe44e7b18f246f
Wilifred <127045260+Wilifred@users.noreply.github.com>
2025-10-24 10:29:28 +0200

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char c = "char";
+    short s = 1000;
+    int i = 32;
+    long int li = 4444L;
+    long long int lli = 44444444LL;
+
+    float f = 6.66f;
+    double d = 5.43;
+    long double ld = 3.441414114L;
+
+    printf("char", c);
+    printf("short", s;
+    
+
+    return 0;
+}
