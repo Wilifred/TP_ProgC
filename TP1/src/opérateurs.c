7b02eebc8657dc00a4d8a40f2b812834ae7b92da
Wilifred <127045260+Wilifred@users.noreply.github.com>
2025-10-24 10:59:29 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -7,13 +7,14 @@ int b = 3;
 printf("Addition %d\n",a+b); 
 printf("Soustraction %d\n",a-b);  
 printf("Multiplication %d\n",a*b);  
-printf("Division %d\n",a/b);
+printf("Division %.2f\n",a/b);
 printf("Modulo %d\n",a%b);  
   
   
   
   
 }
+
 
 
 
