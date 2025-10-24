c85750b94ee005f0f05d2f43e1579a921f3cee4d
Wilifred <127045260+Wilifred@users.noreply.github.com>
2025-10-24 11:24:10 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -2,24 +2,20 @@
 
 int main() {
     int compteur = 5; 
-    
-    if (compteur >= 10 || compteur <= 0) {
-        printf("Erreur : compteur doit être strictement inférieur à 10 et positif.\n");
+
+    if (compteur >= 10) {
+        printf("La valeur de compteur doit être strictement inférieure à 10.\n");
         return 1;
     }
 
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
-            if (i == 1 || i == compteur) {
+            if (i % 2 == 0 && j > 1 && j < i)
+                printf("# ");
+            else
                 printf("* ");
-            } else {
-                if (j == 1 || j == i)
-                    printf("* ");
-                else
-                    printf("# ");
-            }
         }
-        printf("\n"); 
+        printf("\n");
     }
 
     return 0;
