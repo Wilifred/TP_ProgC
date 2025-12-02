1012b285c45eed75f9a6f3aaf6b6fc605c4c48fa
Wilfred4 <wilfredmariechristy@outlook.fr>
2025-12-02 10:08:32 +0100

    variables.c Fait

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -11,20 +11,27 @@ int main() {
     double d = 5.43;
     long double ld = 3.441414114L;
 
-    printf("char %c\n", c);
-    printf("short %d\n", s);
-    printf("int %d\n", i);
-    printf("long int %ld\n", li);
-    printf("long long int %lld\n", lli);
+    unsigned char uc = 200;
+    unsigned short us = 50000;
+    unsigned int ui = 3000000000U;
+    unsigned long int uli = 999999999UL;
+    unsigned long long int ulli = 18446744073709551615ULL;
 
-    printf("float %.2f\n",f);
-    printf("double %.15lf\n",d);
-    printf("long double %.18Lf\n", ld);
+    printf("char = %c\n", c);
+    printf("short = %d\n", s);
+    printf("int = %d\n", i);
+    printf("long int = %ld\n", li);
+    printf("long long int = %lld\n", lli);
+
+    printf("float = %.2f\n", f);
+    printf("double = %.15lf\n", d);
+    printf("long double = %.18Lf\n", ld);
+
+    printf("unsigned char = %u\n", uc);
+    printf("unsigned short = %hu\n", us);
+    printf("unsigned int = %u\n", ui);
+    printf("unsigned long int = %lu\n", uli);
+    printf("unsigned long long int = %llu\n", ulli);
 
     return 0;
 }
-
-
-
-
-
